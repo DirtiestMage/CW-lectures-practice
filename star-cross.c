f9736f91b3ec02bf28a88426b96c30bc5933086e
Aditya Jha <78aaditya@gmail.com>
2025-11-09 19:24:15 +0530

    star cross done

---
+++ star-cross.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("enter the star radius: ");
+    scanf("%d",&n);
+    n = n*2+1;
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++){
+            if (i+j==n+1 || i==j){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");   
+    }
+
+    return 0;
+}
